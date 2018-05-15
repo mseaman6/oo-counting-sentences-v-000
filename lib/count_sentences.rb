3780afd825d46d41e446af02ac21e7784e43111c
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-15 15:25:08 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    complex_string = self.split(". ")
+    counter = complex_string.count
+      complex_string.each do |string|
+        if string.include? "! "
+          counter += string.split("! ").count
+        elsif string.include? "? "
+          counter += string.split("? ").count
+        else
+        end
+      end
+    counter
   end
 end
